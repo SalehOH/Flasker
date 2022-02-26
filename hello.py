259e762c96f937d2b207a321b0f085e853a2efad
Saleh <Salhussaini0@gmail.com>
2022-02-26 17:57:46 +0000

    initial commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+# flask instance
+app = Flask(__name__)
+
+# Create route decoratry
+@app.route('/')
+#def index():
+#    return "<h1> Hello World!! </h1>"
+def index():
+    return render_template("index.html")
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", name=name)
+
+#create erorr pages 
+
+#invalid url
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# internal server Erorr
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("505-html"), 500
